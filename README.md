0413a7babc753fb97c300ef902c3de278b6edeb7
Gyu-Ho Lee <gyuhox@gmail.com>
2016-03-27 13:26:26 -0700

    *: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# llrb
+## llrb [![Build Status](https://img.shields.io/travis/gyuho/llrb.svg?style=flat-square)](https://travis-ci.org/gyuho/llrb) [![Godoc](http://img.shields.io/badge/go-documentation-blue.svg?style=flat-square)](https://godoc.org/github.com/gyuho/llrb)
+
 Package llrb implements left-leaning Red-Black tree.
---
+++ doc.go
@@ -0,0 +1,15 @@
+// Package llrb implements a Left-Leaning Red-Black tree,
+// which is a 2-3 balanced binary search tree:
+//	* Every node(or edge) is either black or red.
+//	* Every path from root to null Node has the same number of black nodes.
+//	* Red nodes lean left.
+//	* Two red nodes in a row are not allowed.
+//
+// Reference
+//	* https://github.com/petar/GoLLRB
+//	* http://www.cs.princeton.edu/~rs/talks/LLRB/08Penn.pdf
+//	* http://www.cs.princeton.edu/~rs/talks/LLRB/LLRB.pdf
+//	* https://www.cs.princeton.edu/~rs/talks/LLRB/RedBlack.pdf
+//	* https://www.youtube.com/watch?v=lKmLBOJXZHI
+//
+package llrb // import "github.com/gyuho/llrb"
